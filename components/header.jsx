c09f0874e7b8a4fa748911d4ebee5fbd37814b16
Paul <paul@theapplegates.com>
2024-06-25 15:08:07 -0400

    zz

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import netlifyLogo from 'public/netlify-logo.svg';
-import githubLogo from 'public/images/github-mark-white.svg';
+// import githubLogo from 'public/images/github-mark-white.svg';
 
 const navItems = [
     { linkText: 'Home', href: '/' },
